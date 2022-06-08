ca9198e0624f62c1d143b731d2c0ad18472bb635
detextre4 <detextre4@gmail.com>
2022-06-08 14:25:59 -0400

    upte

--- src/Routes.js
+++ src/Routes.js
@@ -47,7 +47,7 @@ export default new Router({
         component: () => import('@/pages/Tienda/TiendaNew'),
       },
       {
-        path: '/Buywithstripeclaimyourticket',
+        path: '/Y2xhaW1uZnQ',
         name: 'claim',
         component: () => import('@/pages/Tienda/Claim'),
       },
