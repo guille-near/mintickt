ce43fddb853060d66105f808245401005ad6d20b
detextre4 <detextre4@gmail.com>
2022-06-08 14:23:16 -0400

    update

--- src/Routes.js
+++ src/Routes.js
@@ -47,7 +47,7 @@ export default new Router({
         component: () => import('@/pages/Tienda/TiendaNew'),
       },
       {
-        path: '/claim',
+        path: '/Buywithstripeclaimyourticket',
         name: 'claim',
         component: () => import('@/pages/Tienda/Claim'),
       },
