4e8ca647e5f7e796fbf7a5e8950c3965ae3817de
ccoronel7 <ccoronel@dvconsultores.com>
2022-06-15 19:31:49 -0400

    modificaciones

--- src/pages/Tienda/Tienda.vue
+++ src/pages/Tienda/Tienda.vue
@@ -2,11 +2,11 @@
   <section id="tienda" class="center align divcol">
     <aside>
       <!-- new -->
-      <img class="eliminarmobile" src="@/assets/img/bg_flee.png" alt="Background Image">
-      <img class="vermobile" src="@/assets/img/bg_flee-mobile.png" alt="Background Image">
+      <img class="eliminarmobile" src="@/assets/img/120bpm_desktop.png" alt="Background Image">
+      <img class="vermobile" src="@/assets/img/120bpm_mobile.png" alt="Background Image">
       <!-- new -->
       <div class="fill-w limiter align">
-        <v-chip color="rgba(0, 0, 0, 0.3)">JUN 20</v-chip>
+        <v-chip color="rgba(0, 0, 0, 0.3)">JUN 21</v-chip>
         <h2>{{tittle}}</h2>
         <span>Black Cat Cinema</span>
       </div>
@@ -75,7 +75,7 @@
       </div>
 
       <article id="buy" class="divcol acenter" v-intersect="onIntersect" >
-         <img src="@/assets/img/ticket_flee.svg" alt="Ticket" />
+         <img src="@/assets/img/120bpm_ticket.png" alt="Ticket" />
         <div class="contenedor_aside divcol fill-w">
         <aside class="divrow">
           <span class="h8-em space" style="width: 100%; gap: .5em">
@@ -156,7 +156,7 @@ export default {
   apollo: {
     things_by_pk: {
        query: gql`{
-  things_by_pk(id: "1g69uL5UfbdIZVIajiu3Tpo44ypZY2H88pN5VHpcriE:mintickt.mintbase1.near") {
+  things_by_pk(id: "crOw6WeCbB0ZaSXLOAVnJk0CAVKA3ClwSMW1rEYY1kY:mintickt.mintbase1.near") {
     id
     metadata {
       category
@@ -204,7 +204,7 @@ export default {
             text: "<p>Arroz Estúdios is a members-only, non-profit Cultural Association based in the Beato area of East Lisbon. If you're not already a member of the club, a 1-year membership is just €3 (cash only) at the door.</p>Food, drinks, and popcorn are available before, during, and after the film. The bar has a wide range of beer, wine, cocktails, and soft drinks. The kitchen serves delicious, authentically-made wood fire pizza. Please note, it's cash only for pizza and popcorn!",
           },
           movie: {
-            text: "<p><strong>Plot: </strong>To honour the strength and courage of refugees worldwide and to stimulate public awareness and support for refugees, Black Cat Cinema hosts this special event on World Refugee Day with a screening of ‘Flee’. This 2021 animated Danish documentary follows the story of a man under the alias Amin Nawabi, who shares his hidden past of fleeing his country Afghanistan to Denmark for the first time.</p>The film received a nomination at the Academy Awards for Best International Feature Film, along with nominations in the Best Documentary and Best Animated Feature categories, becoming the first film in history to be nominated in all three major categories simultaneously. Besides this, ‘Flee’ won multiple international awards, including Grand Jury Prize in the World Cinema at Sundance Film Festival, and has been considered as one of the best movies of 2021.",
+            text: "<p><strong>Plot: </strong>As the AIDS epidemic tears through their community during the early 90’s, the members of ACT UP Paris are fighting for survival. While they battle against governmental apathy, pharmaceutical greed and public ignorance, they also go out partying, debate politics and, occasionally, fall in love. One day, as outspoken radical Sean (Nahuel Pérez Biscayart) strikes up a conversation with shy newcomer Nathan (Arnaud Valois), they have no idea that their lives are about to change forever.",
           },
           info: [
             {
@@ -234,7 +234,7 @@ export default {
           },
           {
             titlesDetails: "Transactions ID",
-            textDetails: "1g69uL5UfbdIZVIajiu3Tpo44ypZY2H88pN5VHpcriE",
+            textDetails: "crOw6WeCbB0ZaSXLOAVnJk0CAVKA3ClwSMW1rEYY1kY",
           },
           {
             titlesDetails: "Contract",
@@ -242,7 +242,7 @@ export default {
           },
           {
             titlesDetails: "Thing ID",
-            textDetails: "1g69uL5UfbdIZVIajiu3Tpo44ypZY2H88pN5VHpcriE:mintickt.mintbase1.near",
+            textDetails: "crOw6WeCbB0ZaSXLOAVnJk0CAVKA3ClwSMW1rEYY1kY:mintickt.mintbase1.near",
           },
         ]
       },
@@ -279,10 +279,10 @@ export default {
     if (urlParams.get("transactionHashes") !== null) {
       console.log('aqui' + urlParams.get("transactionHashes"))
       this.dialog = true
-      history.replaceState(null, location.href.split("?")[0], '/events/ZJdegansubNv80mSfHKGYbabAYZdkQ3vd7lzQ-Sb27U:mintickt.mintbase1.near/#/');
+      history.replaceState(null, location.href.split("?")[0], '/events/crOw6WeCbB0ZaSXLOAVnJk0CAVKA3ClwSMW1rEYY1kY:mintickt.mintbase1.near/#/');
     }
     if (urlParams.get("errorCode") !== null) {
-      history.replaceState(null, location.href.split("?")[0], '/events/ZJdegansubNv80mSfHKGYbabAYZdkQ3vd7lzQ-Sb27U:mintickt.mintbase1.near/#/');
+      history.replaceState(null, location.href.split("?")[0], '/events/crOw6WeCbB0ZaSXLOAVnJk0CAVKA3ClwSMW1rEYY1kY:mintickt.mintbase1.near/#/');
     }
   },
   methods: {
@@ -293,9 +293,7 @@ export default {
       console.log(amountInYocto);      return amountInYocto;
     },
    async traerdatos(){
-        console.log(this.things_by_pk)
-        this.duneticket ="https://arweave.net/LtKJYD5aoy5D2DX8H-va1soi_iHpRxgEDapZWMDs1TQ"
-        this.tittle =  this.things_by_pk.metadata.title
+        this.tittle =  "120 BPM"
         this.tokens_totales =  this.things_by_pk.tokens.length
         // this.location = this.things_by_pk.metadata.extra.location.value
         this.cantidad_disponible = 0
