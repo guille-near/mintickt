6e8694b52fae6e60a2aa91899b1e811dc9b11c37
detextre4 <detextre4@gmail.com>
2022-06-29 13:43:17 -0400

    update

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -11,14 +11,20 @@
         <v-col class="space" :style="route=='Events'||route=='EventsNearcon'?
           'padding:0':'padding-inline: clamp(1em, 4vw, 4em)'">
           <a class="center" href="/">
-            <img class="logoHeader" src="@/assets/logo/logom.svg" alt="logo">
+            <img v-if="responsiveActions" class="logoHeaderEvents" src="@/assets/logo/logo-mobile.svg" alt="logo">
+            <img v-else class="logoHeader" src="@/assets/logo/logom.svg" alt="logo">
           </a>
           
           <aside class="acenter" style="gap:.2em" :style="route=='Events'||route=='EventsNearcon'?'':'display:contents'">
             <v-btn class="createEventBtn h9-em" v-if="route=='Events'||route=='EventsNearcon'">
               <span>create an event</span>
             </v-btn>
-            <v-btn text color="white" rounded class="h9-em" @click="connect">
+
+            <v-btn v-if="responsiveActions"
+              color="white" class="btnNearMobile h7-em" @click="connect">
+              <img src="@/assets/logo/near-black.svg" alt="near logo">
+            </v-btn>
+            <v-btn v-else text color="white" rounded class="h9-em" @click="connect">
               <img src="@/assets/logo/near.svg" alt="near">
               <span>{{user}}</span>
             </v-btn>
@@ -65,16 +71,22 @@ export default {
       // themeButton: true,
       nearid: false,
       user: 'Login with NEAR',
+      responsiveActions: false,
     };
   },
   mounted(){
     this.revisar()
+    this.responsive()
+    // document.addEventListener('resize', this.responsive())
   },
   computed: {
     route() {return this.$router.currentRoute.name}
   },
   methods: {
-  
+    responsive() {
+      if (window.innerWidth <= 880 && this.route=='Events'||this.route=='EventsNearcon') {this.responsiveActions = true}
+      else {this.responsiveActions = false}
+    },
   async  connect() {
   let API_KEY = "c45bd9f9-b880-4e1e-9ecb-c71309348a7d"
   const { data: walletData } = await new Wallet().init({
