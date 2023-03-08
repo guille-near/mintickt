93812cab3f77f18ceec707769751a8353dc5e5fd
dvconsultores <adominguez@dvconsultores.com>
2023-03-08 16:59:48 -0400

    .

--- src/pages/Events/RegisterDashboard.vue
+++ src/pages/Events/RegisterDashboard.vue
@@ -1807,7 +1807,7 @@ export default {
               backgroundColor: null,
               allowTaint: true,
               removeContainer: true,
-              scale: 5,
+              scale: 10,
               quality: 1 // Set the maximum quality
             };
             html2canvas(container, options).then((canvas) => {
